2e5b819cee47c5907279a44809e7e70daeb38e3b
Gandha Ryanto <ganda@mdd.co.id>
2025-05-21 02:32:59 +0700

    Add setupAssociations call to initialize model relationships

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,8 @@
 const Student = require('./Student');
 const Attendance = require('./Attendance');
+const setupAssociations = require('./associations');
+
+setupAssociations();
 
 module.exports = {
   Student,
